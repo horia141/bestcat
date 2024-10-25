c8178f67801b0f5b84238f81de8dd1d75e87452e
Horia Coman <horia141@gmail.com>
2024-10-25 23:12:12 +0300

    Win dialog shows a retry

--- src/game/ui/win-dialog/win_dialog.gd
+++ src/game/ui/win-dialog/win_dialog.gd
@@ -1,13 +1,17 @@
 class_name WinDialog
 extends Dialog
 
-signal continue_after_winning ()
+signal retry_mission()
+signal continue_after_winning()
 
 #region Game logic
 
 func activate() -> void:
 	super.activate()
 	$Contents/Controls/Continue.grab_focus()
+	
+func _retry() -> void:
+	retry_mission.emit()
 
 func _continue() -> void:
 	continue_after_winning.emit()
