4232d9bd969fd8d53fa9e468ed6f5e265f197b23
Horia Coman <horia141@gmail.com>
2024-10-28 19:03:00 +0200

    A new enemy mob - the snail

---
+++ src/game/entities/enemies/mobs/snail/snail.gd
@@ -0,0 +1,21 @@
+class_name Snail
+extends Mob
+
+const EnemyProjectileScn = preload("res://entities/enemies/projectile/enemy-projectile.tscn")
+
+static var SHOOT_PERIOD_SEC = DifficultyValue.new(1, 0.9, 0.8)
+
+#region Game logic
+
+func _shoot() -> void:
+	if state != EnemyState.Active:
+		return
+
+	var enemy_projectile = EnemyProjectileScn.instantiate()
+	enemy_projectile.post_ready_prepare(position, scale, Vector2(-1, 0).rotated(randf_range(0, TAU)), difficulty)
+	shoot.emit(enemy_projectile)
+	
+	$ShootTimer.wait_time = SHOOT_PERIOD_SEC.get_for(difficulty) + randf_range(-0.25, 0.25)
+	$ShootTimer.start()
+
+#endregion
