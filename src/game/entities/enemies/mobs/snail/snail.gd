dab03aefc82e30f96a21ae9adceb2afcfa92ea85
Horia Coman <horia141@gmail.com>
2024-11-03 18:34:10 +0200

    A bit more randomness in snail shot

--- src/game/entities/enemies/mobs/snail/snail.gd
+++ src/game/entities/enemies/mobs/snail/snail.gd
@@ -11,8 +11,12 @@ func _shoot() -> void:
 	if state != EnemyState.Active:
 		return
 		
-	var enemy_projectile = TranquilizerScn .instantiate()
-	enemy_projectile.post_ready_prepare(position, scale, position.direction_to(player.position), difficulty)
+	var enemy_projectile = TranquilizerScn.instantiate()
+	enemy_projectile.post_ready_prepare(
+		position, 
+		scale, 
+		position.direction_to(player.position).rotated(randf_range(-0.2, 0.2)), 
+		difficulty)
 	shoot.emit(enemy_projectile)
 	
 	$ShootTimer.wait_time = SHOOT_PERIOD_SEC.get_for(difficulty) + randf_range(-0.25, 0.25)
