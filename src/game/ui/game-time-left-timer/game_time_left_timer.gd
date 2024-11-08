7fd5c1eee90b79a59da4b0ab1d30bf5d6d542146
Horia Coman <horia141@gmail.com>
2024-11-08 19:16:54 +0200

    Add nice animations when showing the time running out

--- src/game/ui/game-time-left-timer/game_time_left_timer.gd
+++ src/game/ui/game-time-left-timer/game_time_left_timer.gd
@@ -3,9 +3,15 @@ extends CanvasLayer
 
 signal game_time_expired ()
 
+const WARNING_CUTOFF_SEC = 30
+const WARNING_COLOR = Color.ORANGE
+const DANGER_CUTOFF_SEC = 10
+const DANGER_COLOR = Color.RED
 static var INITIAL_GAME_TIME = DifficultyValue.new(180, 150, 120)
 
 var game_time_left_sec = INITIAL_GAME_TIME.get_for(Application.MissionDifficulty.Apprentice)
+var showing_warning_animation = false
+var showing_danger_animation = false
 
 #region Construction
 
@@ -32,7 +38,24 @@ func _on_timer_timeout() -> void:
 	game_time_left_sec = game_time_left_sec - 1
 	__update_timer()
 	
+	if game_time_left_sec <= WARNING_CUTOFF_SEC and not showing_warning_animation: 
+		var tween = get_tree().create_tween()
+		tween.bind_node($Text)
+		tween.tween_property($Text, "modulate", WARNING_COLOR, 0.5)
+		tween.chain().tween_property($Text, "modulate", Color.WHITE, 0.5)
+		tween.set_loops(WARNING_CUTOFF_SEC - DANGER_CUTOFF_SEC)
+		showing_warning_animation = true
+	
+	if game_time_left_sec <= 10 and not showing_danger_animation:
+		var tween = get_tree().create_tween()
+		tween.bind_node($Text)
+		tween.tween_property($Text, "modulate", DANGER_COLOR, 0.5)
+		tween.chain().tween_property($Text, "modulate", Color.WHITE, 0.5)
+		tween.set_loops(DANGER_CUTOFF_SEC)
+		showing_danger_animation = true
+	
 	if game_time_left_sec == 0:
+		hide()
 		$Timer.stop()
 		game_time_expired.emit()
 		
