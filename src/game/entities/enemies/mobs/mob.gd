c0e6421bb9e382f6336ca6bb2663eada46a0b2bd
Horia Coman <horia141@gmail.com>
2024-11-16 16:03:05 +0200

    Nice stuff on activation & deactivation of mobs

--- src/game/entities/enemies/mobs/mob.gd
+++ src/game/entities/enemies/mobs/mob.gd
@@ -1,10 +1,24 @@
 class_name Mob
 extends Enemy
 
+const SleepSignScn = preload("res://entities/enemies/sleep-sign/sleep-sign.tscn")
+
+var activation_tween: Tween = null
+var sleep_sign = null
+
 #region Construction
 
 func post_ready_prepare(player: Game.PlayerProxy, init_position: Vector2, difficulty: Application.MissionDifficulty) -> void:
 	super.post_ready_prepare(player, init_position, difficulty)
+	
+	sleep_sign = SleepSignScn.instantiate()
+	sleep_sign.scale = sleep_sign.scale / self.scale
+	sleep_sign.position = Vector2($Collision.shape.get_rect().position + Vector2($Collision.shape.get_rect().size.x, 0))
+	
+	
+	add_child(sleep_sign)
+	$Sprite.stop()
+	init_tween.chain().tween_property(self, "modulate", Color.GRAY, 0.2)
 	
 	var immediate_activation = true
 	for child in get_children():
@@ -12,10 +26,10 @@ func post_ready_prepare(player: Game.PlayerProxy, init_position: Vector2, diffic
 			child.player_entered.connect(_player_entered_activation_area)
 			child.player_exited.connect(_player_exited_activation_area)
 			immediate_activation = false
-			return
-			
+			break
+		
 	if immediate_activation:
-		state = EnemyState.Active
+		_player_entered_activation_area(null)
 
 #endregion
 
@@ -27,11 +41,22 @@ func _player_entered_activation_area(player: Player) -> void:
 	state = EnemyState.Active
 	$ShootTimer.wait_time = 0.1
 	$ShootTimer.start()
+	$Sprite.play("idle")
+	
+	activation_tween = create_tween()
+	activation_tween.tween_property(self, "modulate", Color.WHITE, 0.2)
+	
+	remove_child(sleep_sign)
 	
 func _player_exited_activation_area(player: Player) -> void:
 	if state == EnemyState.Hidden or state == EnemyState.Dead:
 		return
 	state = EnemyState.Inactive
+	
+	add_child(sleep_sign)
+	$Sprite.stop()
+	activation_tween = create_tween()
+	activation_tween.tween_property(self, "modulate", Color.GRAY, 0.2)
 
 func on_hit_by_projectile() -> void:
 	super.on_hit_by_projectile()
@@ -48,6 +73,8 @@ func destroy() -> void:
 	set_deferred("freeze", true)
 	await $Sprite.animation_finished
 	destroyed.emit()
+	if activation_tween != null:
+		activation_tween.kill()
 	
 func is_bound_to_dark_tower() -> bool:
 	return true
